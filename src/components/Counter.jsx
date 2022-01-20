f837e773fe429bfb66a6382fcd8043e5685ead96
akancodes <akancodes@gmail.com>
2022-01-20 06:21:08 +0300

    added authentication reducer

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,13 @@
+import { Fragment } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { counterActions } from "../store/index";
+import { counterActions } from "../store/counter";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector((state) => state.counter);
-  const show = useSelector((state) => state.showCounter);
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.counter.showCounter);
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
 
   const incrementHandler = () => {
     dispatch(counterActions.increment());
@@ -24,16 +26,20 @@ const Counter = () => {
   };
 
   return (
-    <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      {show && <div className={classes.value}>-- {counter} --</div>}
-      <div>
-        <button onClick={incrementHandler}>Increment</button>
-        <button onClick={increaseHandler}>Increment by 10</button>
-        <button onClick={decrementHandler}>Decrement</button>
-      </div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
-    </main>
+    <Fragment>
+      {isAuth && (
+        <main className={classes.counter}>
+          <h1>Redux Counter</h1>
+          {show && <div className={classes.value}>-- {counter} --</div>}
+          <div>
+            <button onClick={incrementHandler}>Increment</button>
+            <button onClick={increaseHandler}>Increment by 10</button>
+            <button onClick={decrementHandler}>Decrement</button>
+          </div>
+          <button onClick={toggleCounterHandler}>Toggle Counter</button>
+        </main>
+      )}
+    </Fragment>
   );
 };
 
